6b86cf9f9f2157d4eb98cfe3b891e1ab212f0079
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-05-08 18:34:54 +0100

    fix: image size adjusted

--- app/components/ImageCard.jsx
+++ app/components/ImageCard.jsx
@@ -4,7 +4,7 @@ const ImageCard = ({ src, alt, title }) => {
     return (
         <div className='flex flex-col transform transition-transform duration-500 hover:scale-105'>
             <img src={src} alt={alt} />
-            {/* <span className='mt-2 text-xs md:text-base font-semibold text-black uppercase'>{title}</span> */}
+            <span className='mt-2 text-black uppercase'>{title}</span>
         </div>
     );
 }
--- app/fabricSamples/page.jsx
+++ app/fabricSamples/page.jsx
@@ -14,8 +14,7 @@ const fabricSamples = () => {
                     <div className="grid grid-cols-2 md:grid-cols-1 gap-6 md:gap-16 p-6 md:p-16">
                         {fabricSamplesData.map((sample, index) => (
                             <Link href={`/fabricSamples/${index}`} key={index}>
-                                <ImageCard src={sample.src} alt={sample.alt} title={sample.title} />
-                                <span className='text-black'>{index}</span>
+                                <ImageCard src={sample.src} alt={sample.alt} title={index+1} />
                             </Link>
                         ))}
                     </div>
