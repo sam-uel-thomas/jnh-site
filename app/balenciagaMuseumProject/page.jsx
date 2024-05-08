06408da070c93005bb70ab5ea325dd27043e95ce
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-05-08 18:14:19 +0100

    fix: test on module imports

--- app/balenciagaMuseumProject/page.jsx
+++ app/balenciagaMuseumProject/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+// import Navbar from '../components/Navbar'
+// import Footer from '../components/Footer'
 import ImageCard from '../components/ImageCard'
 
 const balenciagaMuseumProjectData = [];
@@ -9,7 +9,7 @@ const BalenciagaMuseumProject = () => {
     return (
         <div>
             <div className='bg-white flex flex-col justify-between min-h-[100dvh]'>
-                <Navbar />
+                {/* <Navbar /> */}
                 <div className="relative flex flex-col justify-end items-center w-full pt-0 md:pt-8 mb-5 z-10">
                     <div className="grid grid-cols-2 md:grid-cols-3 gap-6 md:gap-16 p-6 md:p-16">
                         {balenciagaMuseumProjectData.length > 0 ? (
@@ -22,7 +22,7 @@ const BalenciagaMuseumProject = () => {
                     </div>
                 </div>
             </div>
-            <Footer />
+            {/* <Footer /> */}
         </div>
     );
 }
