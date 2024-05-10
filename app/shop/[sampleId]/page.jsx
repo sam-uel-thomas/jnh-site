27845721cea24af75fc3a61350443e73c9669e33
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-05-10 15:00:55 +0100

    feat: multiple shop images

--- app/shop/[sampleId]/page.jsx
+++ app/shop/[sampleId]/page.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import shopData from '../shopData'
 import ImageCard from '../../components/ImageCard'
 import Navbar from '../../components/Navbar';
@@ -7,18 +8,29 @@ import Link from 'next/link'
 
 export default function page({ params }) {
     const sample = shopData[Number(params.sampleId)];
+    const [photoIndex, setPhotoIndex] = useState(0);
 
     if (!sample) {
         return <h1>No fabric sample found for id {params.sampleId}</h1>;
     }
 
+    const handleNextPhoto = () => {
+        setPhotoIndex((prevIndex) => (prevIndex + 1) % sample.photos.length);
+    };
+
+    const handlePrevPhoto = () => {
+        setPhotoIndex((prevIndex) => (prevIndex - 1 + sample.photos.length) % sample.photos.length);
+    };
+
     return (
         <>
             <div className='bg-white flex flex-col min-h-screen'>
                 <Navbar />
-                <div className="pl-32 mt-12 flex items-start justify-start">
-                    <div className='w-[28rem] h-auto mr-8'>
-                        <ImageCard src={sample.src} alt={sample.alt}/>
+                <div className="pl-32 mt-12 h-[70vh] flex items-start justify-start">
+                    <div className='relative w-[24rem] h-auto mr-8'>
+                        <ImageCard src={sample.photos[photoIndex]} alt={sample.alt}/>
+                        <button onClick={handlePrevPhoto} className="bg-red-500 rounded-full w-12 h-12 absolute top-1/2 left-0 transform -translate-x-1/2 -translate-y-1/2"></button>
+                        <button onClick={handleNextPhoto} className="bg-red-500 rounded-full w-12 h-12 absolute top-1/2 right-0 transform translate-x-1/2 -translate-y-1/2"></button>
                     </div>
                     <div className='flex flex-col w-1/2 mt-16'>
                         <span className='text-black font-semibold text-2xl mb-4'>{sample.title}</span>
--- app/shop/shopData.jsx
+++ app/shop/shopData.jsx
@@ -1,11 +1,24 @@
 const shopData = [
-    {src : "/shop/bagForLifeTrouser/cutout.png", alt: "Shop 1", title : "Bag For Life Trouser", description: ["- Contrast white pocket", "- Perfect size to keep your phone in one pocket and have everything else in the other", "- Makes your bum look good x", "- Like really good x"]},
-    {src : "/shop/fencingVest/cutout.png", alt: "Shop 2", title : "Fencing Vest", description: ["- Like the shirt but black and without sleeves", "- So many lovely buttons my goodness", "- Is that a contrast welt pocket"]},
-    {src : "/shop/horseRiderJacket/cutout.png", alt: "Shop 3", title : "Horse Rider Jacket", description: ["- Wool looks so soft", "- Goodness I just want to stroke it", "- But not in a weird way", "- Love that contrast welt pocket"]},
-    {src : "/shop/whiteFencingShirt/cutout.png", alt: "Shop 7", title : "White Fencing Shirt", description: ["- White fencing jacket", "- Pretty swag if you ask me", "- Lovely buttons"]},
-    {src : "/shop/imperialJacket/cutout.jpg", alt: "Shop 4", title : "Imperial Jacket", description: ["- Goodness he looks great in that", "- Ninja", "- Can't guarantee the secrecy of a ninja", "- Might just sneak you a biscuit from your nans tin"]},
-    {src : "/shop/silverDress/cutout.png", alt: "Shop 5", title : "Silver Dress", description: ["- Futuristic mini dress", "- Future reflects so bright in this", "- Great stripes"]},
-    {src : "/shop/targetPractiseTrousers/cutout.jpg", alt: "Shop 6", title : "Target Practise Trousers", description: ["- 100% of the shots you don't take you miss", "- But you won't miss in these", "- 100% guaranteed", "- Okay maybe 85%"]},
+    {src : "/shop/bagForLifeTrouser/cutout.png", alt: "Shop 1", title : "Bag For Life Trouser", description: ["- Contrast white pocket", "- Perfect size to keep your phone in one pocket and have everything else in the other", "- Makes your bum look good x", "- Like really good x"],
+    photos: ["/shop/bagForLifeTrouser/cutout.png", "/shop/bagForLifeTrouser/2.JPG", "/shop/bagForLifeTrouser/3.JPG", "/shop/bagForLifeTrouser/4.JPG"]},
+
+    {src : "/shop/fencingVest/cutout.png", alt: "Shop 2", title : "Fencing Vest", description: ["- Like the shirt but black and without sleeves", "- So many lovely buttons my goodness", "- Is that a contrast welt pocket"],
+    photos: ["/shop/fencingVest/cutout.png", "/shop/fencingVest/2.JPG", "/shop/fencingVest/3.JPG", "/shop/fencingVest/4.JPG"]},
+
+    {src : "/shop/horseRiderJacket/cutout.png", alt: "Shop 3", title : "Horse Rider Jacket", description: ["- Wool looks so soft", "- Goodness I just want to stroke it", "- But not in a weird way", "- Love that contrast welt pocket"],
+    photos: ["/shop/horseRiderJacket/cutout.png", "/shop/horseRiderJacket/2.JPG", "/shop/horseRiderJacket/3.JPG", "/shop/horseRiderJacket/4.JPG"]},
+
+    {src : "/shop/whiteFencingShirt/cutout.png", alt: "Shop 7", title : "White Fencing Shirt", description: ["- White fencing jacket", "- Pretty swag if you ask me", "- Lovely buttons"],
+    photos: ["/shop/whiteFencingShirt/cutout.png", "/shop/whiteFencingShirt/2.JPG", "/shop/whiteFencingShirt/3.JPG"]},
+
+    {src : "/shop/imperialJacket/cutout.jpg", alt: "Shop 4", title : "Imperial Jacket", description: ["- Goodness he looks great in that", "- Ninja", "- Can't guarantee the secrecy of a ninja", "- Might just sneak you a biscuit from your nans tin"],
+    photos: ["/shop/imperialJacket/cutout.jpg", "/shop/imperialJacket/2.JPG", "/shop/imperialJacket/3.JPG", "/shop/imperialJacket/4.JPG"]},
+
+    {src : "/shop/silverDress/cutout.png", alt: "Shop 5", title : "Silver Dress", description: ["- Futuristic mini dress", "- Future reflects so bright in this", "- Great stripes"],
+    photos: ["/shop/silverDress/cutout.png", "/shop/silverDress/2.JPG", "/shop/silverDress/3.JPG"]},
+
+    {src : "/shop/targetPractiseTrousers/cutout.jpg", alt: "Shop 6", title : "Target Practise Trousers", description: ["- 100% of the shots you don't take you miss", "- But you won't miss in these", "- 100% guaranteed", "- Okay maybe 85%"],
+    photos: ["/shop/targetPractiseTrousers/cutout.jpg", "/shop/targetPractiseTrousers/2.JPG", "/shop/targetPractiseTrousers/3.JPG", "/shop/targetPractiseTrousers/4.JPG"]},
 ];
 
 export default shopData;
