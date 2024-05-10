740c411d4d2e5c1645a033fc61995c4167a8583f
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-05-10 01:23:10 +0100

    feat: add portfolio

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Link from 'next/link'
 
 const Portfolio = () => {
     return (
         <div>
-            <div className='bg-white flex flex-col justify-between min-h-[100dvh]'>
-                <Navbar />
-                <div className="relative flex flex-col justify-end items-center w-full pt-0 md:pt-8 mb-5 z-10">
+                <div className='bg-white flex flex-col min-h-screen'>
+                    <Navbar />
+                    <div className='justify-center items-center w-full pt-0 md:pt-8 mb-5 z-10'>
+                        <div className="relative flex flex-col items-center w-full pt-0 md:pt-8 mb-5 z-10">
+                            <ul className="space-y-4">
+                                <li>
+                                    <Link href="/portfolio/mini.pdf">
+                                        <span className='text-black font-semibold'>MINI PORTFOLIO</span>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/portfolio/main.pdf">
+                                        <span className='text-black font-semibold'>MAIN PORTFOLIO</span>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/portfolio/print.pdf">
+                                        <span className='text-black font-semibold'>PRINT PORTFOLIO</span>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-            </div>
             <Footer />
         </div>
     );
---
+++ app/shop/shopData.jsx
@@ -0,0 +1,13 @@
+const shopData = [
+    { src: "/shop/1.JPG", alt: "Print 1"},
+    { src: "/shop/2.JPG", alt: "Print 2"},
+    { src: "/shop/3.JPG", alt: "Print 3"},
+    { src: "/shop/4.JPG", alt: "Print 4"},
+    { src: "/shop/5.JPG", alt: "Print 5"},
+    { src: "/shop/6.JPG", alt: "Print 6"},
+    { src: "/shop/7.JPG", alt: "Print 7"},
+    { src: "/shop/8.PNG", alt: "Print 8"},
+    { src: "/shop/9.PNG", alt: "Print 9"},
+];
+
+export default shopData;
