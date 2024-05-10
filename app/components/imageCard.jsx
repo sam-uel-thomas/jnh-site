e9faad5c91de372217f9840f83f8812402b5f4a6
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-05-10 01:41:22 +0100

    fix: adjust img to Image

--- app/components/imageCard.jsx
+++ app/components/imageCard.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Image from 'next/image';
 
 const ImageCard = ({ src, alt, title }) => {
     return (
         <div className='flex flex-col transform transition-transform duration-500 hover:scale-105'>
-            <img src={src} alt={alt} />
+            <Image
+                src={src}
+                alt={alt}
+                width={1080}
+                height={1920}
+            />
             <span className='mt-2 text-black uppercase'>{title}</span>
         </div>
     );
