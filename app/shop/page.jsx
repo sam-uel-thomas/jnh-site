c48dfd2f492b44160429dd8ebb054a1900888ab0
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-05-10 02:12:51 +0100

    feat: add shop items

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import ImageCard from '../components/ImageCard'
+import shopData from './shopData'
+import Link from 'next/link'
 
-const Shop = () => {
+const shop = () => {
     return (
         <div>
             <div className='bg-white flex flex-col justify-between min-h-[100dvh]'>
                 <Navbar />
-                <div className="relative flex flex-col justify-end items-center w-full pt-0 md:pt-8 mb-5 z-10">
-                    <span className='text-2xl text-black font-bold'>Shop</span>
+                <div className="relative flex flex-col justify-end items-center w-full pt-0 md:pt-0 mb-5 z-10">
+                    <div className="grid grid-cols-2 gap-6 md:gap-12 p-6 md:p-16 justify-center">
+                        {shopData.length > 0 ? (
+                            shopData.map((sample, index) => (
+                                <Link href={`/shop/${index}`} key={index} className='flex justify-center items-center'>
+                                    <div className="w-1/2 h-auto">
+                                        <ImageCard src={sample.src} alt={sample.alt} title={`${index+1}: ${sample.title}`} />
+                                    </div>
+                                </Link>
+                            ))
+                        ) : (
+                            <p>No data available</p>
+                        )}
+                    </div>
                 </div>
             </div>
             <Footer />
@@ -16,4 +31,4 @@ const Shop = () => {
     );
 }
 
-export default Shop
+export default shop
--- app/shop/shopData.jsx
+++ app/shop/shopData.jsx
@@ -1,13 +1,11 @@
 const shopData = [
-    { src: "/shop/1.JPG", alt: "Print 1"},
-    { src: "/shop/2.JPG", alt: "Print 2"},
-    { src: "/shop/3.JPG", alt: "Print 3"},
-    { src: "/shop/4.JPG", alt: "Print 4"},
-    { src: "/shop/5.JPG", alt: "Print 5"},
-    { src: "/shop/6.JPG", alt: "Print 6"},
-    { src: "/shop/7.JPG", alt: "Print 7"},
-    { src: "/shop/8.PNG", alt: "Print 8"},
-    { src: "/shop/9.PNG", alt: "Print 9"},
+    {src : "/shop/bagForLifeTrouser/cutout.png", alt: "Shop 1", title : "Bag For Life Trouser"},
+    {src : "/shop/fencingVest/cutout.jpg", alt: "Shop 2", title : "Fencing Vest"},
+    {src : "/shop/horseRiderJacket/cutout.jpg", alt: "Shop 3", title : "Horse Rider Jacket"},
+    {src : "/shop/imperialJacket/cutout.jpg", alt: "Shop 4", title : "Imperial Jacket"},
+    {src : "/shop/silverDress/cutout.jpg", alt: "Shop 5", title : "Silver Dress"},
+    {src : "/shop/targetPractiseTrousers/cutout.jpg", alt: "Shop 6", title : "Target Practise Trousers"},
+    {src : "/shop/whiteFencingShirt/cutout.jpg", alt: "Shop 7", title : "White Fencing Shirt"},
 ];
 
 export default shopData;
