ac5411409cebb58ca8febb99e5d6d20b9efb06de
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-05-10 02:16:10 +0100

    fix: rearrange shop items

--- app/shop/shopData.jsx
+++ app/shop/shopData.jsx
@@ -2,10 +2,10 @@ const shopData = [
     {src : "/shop/bagForLifeTrouser/cutout.png", alt: "Shop 1", title : "Bag For Life Trouser"},
     {src : "/shop/fencingVest/cutout.jpg", alt: "Shop 2", title : "Fencing Vest"},
     {src : "/shop/horseRiderJacket/cutout.jpg", alt: "Shop 3", title : "Horse Rider Jacket"},
+    {src : "/shop/whiteFencingShirt/cutout.jpg", alt: "Shop 7", title : "White Fencing Shirt"},
     {src : "/shop/imperialJacket/cutout.jpg", alt: "Shop 4", title : "Imperial Jacket"},
     {src : "/shop/silverDress/cutout.jpg", alt: "Shop 5", title : "Silver Dress"},
     {src : "/shop/targetPractiseTrousers/cutout.jpg", alt: "Shop 6", title : "Target Practise Trousers"},
-    {src : "/shop/whiteFencingShirt/cutout.jpg", alt: "Shop 7", title : "White Fencing Shirt"},
 ];
 
 export default shopData;
