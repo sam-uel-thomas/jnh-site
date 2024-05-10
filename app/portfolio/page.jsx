dfc062e504d80b022336242dc10069b821d6c896
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-05-10 12:02:28 +0100

    feat: add background shapes to portfolio links

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -2,28 +2,38 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Portfolio = () => {
     return (
         <div>
             <div className='bg-white flex flex-col min-h-screen'>
                 <Navbar />
-                <div className='justify-center items-center w-full pt-0 md:pt-8 mb-5 z-10'>
-                    <div className="relative flex flex-col items-center w-full pt-0 md:pt-8 mb-5 z-10">
-                        <div className="flex flex-row space-x-4">
-                            <div>
+                <div className='justify-center items-center w-full pt-8 md:pt-16 mb-5 z-10'>
+                    <div className="relative flex flex-col items-center w-full pt-16 md:pt-8 mb-5 z-10">
+                        <div className="flex flex-col md:flex-row space-x-4 items-center">
+                            <div className="w-auto h-auto relative">
                                 <Link href="/portfolio/mini.pdf">
-                                    <span className='text-black font-semibold'>MINI PORTFOLIO</span>
+                                    <div className='hover:scale-105 transition-transform duration-500 ease-in-out flex items-center justify-center'>
+                                        <Image src="/logos/portfolioShape1.png" alt="Mini Portfolio" height={400} width={400} />
+                                        <span className='text-black font-semibold absolute'>MINI PORTFOLIO</span>
+                                    </div>
                                 </Link>
                             </div>
-                            <div>
+                            <div className="w-auto h-auto relative">
                                 <Link href="/portfolio/main.pdf">
-                                    <span className='text-black font-semibold'>MAIN PORTFOLIO</span>
+                                    <div className='hover:scale-105 transition-transform duration-500 ease-in-out flex items-center justify-center'>
+                                        <Image src="/logos/portfolioShape2.png" alt="Main Portfolio" height={500} width={500} />
+                                        <span className='text-black font-semibold absolute'>MAIN PORTFOLIO</span>
+                                    </div>
                                 </Link>
                             </div>
-                            <div>
+                            <div className="w-auto h-auto relative">
                                 <Link href="/portfolio/print.pdf">
-                                    <span className='text-black font-semibold'>PRINT PORTFOLIO</span>
+                                    <div className='hover:scale-105 transition-transform duration-500 ease-in-out flex items-center justify-center'>
+                                        <Image src="/logos/portfolioShape3.png" alt="Print Portfolio" height={400} width={400}/>
+                                        <span className='text-black font-semibold absolute'>PRINT PORTFOLIO</span>
+                                    </div>
                                 </Link>
                             </div>
                         </div>
