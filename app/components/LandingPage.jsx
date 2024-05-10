77a1a0104998ffc4eb190993cc2785cc7ab6d2b2
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-05-10 13:27:02 +0100

    fix: animation on landing page

--- app/components/LandingPage.jsx
+++ app/components/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ProjectScroll from './ProjectScroll';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import Image from 'next/image';
 
 const LandingPage = () => {
     return (
@@ -9,8 +10,8 @@ const LandingPage = () => {
             <div className='flex flex-col justify-between h-[100dvh]'>
                 <Navbar />
                 <div className="h-full object-cover mt-4 relative">
-                    <img src="/animation/jnhWalking.gif" alt="JNH Rectangle" className="absolute w-full h-full object-contain object-center -z-2 md:hidden px-2" />
-                    <img src="/animation/8.gif" alt="JNH Rectangle" className="absolute h-full object-contain object-center -z-2 hidden md:block" />
+                    <Image src="/animation/jnhWalking.gif" alt="JNH Rectangle" width={1000} height={1000} className="absolute w-full h-full object-contain object-center -z-2 md:hidden px-2" />
+                    <Image src="/animation/8.GIF" alt="JNH Rectangle" width={2000} height={2000} className="absolute h-full object-contain object-center -z-2 hidden md:block" />
                 </div>
                 <div className="relative flex flex-col justify-end items-center w-full mb-5 z-10">
                     <ProjectScroll/>
