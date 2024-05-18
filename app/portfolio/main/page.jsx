67772f50df72c8d5559e4c51a8202924e00c0c1f
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-05-18 01:24:50 +0100

    add jonahs changes

--- app/portfolio/main/page.jsx
+++ app/portfolio/main/page.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
+import ProjectScroll from '../../components/ProjectScroll';
 
 const images = Array.from({ length: 28 }, (_, i) => `/portfolio/main/${i + 1}.jpg`);
 
@@ -14,22 +15,25 @@ const Page = () => {
 
     return (
         <div>
-            <div className='bg-white flex flex-col items-center min-h-[100dvh]'>
+            <div className='bg-white flex flex-col items-center min-h-[100vh]'>
                 <Navbar />
-                <div className="flex items-center justify-center mx-auto mt-12">
+                <div className="flex items-center justify-center mx-auto flex-grow">
                     <button 
-                        className={`w-24 h-24 mx-8 animate-bobbing-left ${imageNumber === 0 ? 'opacity-50 cursor-default' : ''}`} 
+                        className={`w-32 h-32 mx-8 animate-bobbing-left ${imageNumber === 0 ? 'opacity-50 cursor-default' : ''}`} 
                         onClick={imageNumber > 0 ? goPrevImage : undefined}
                     >
                         <img src="/portfolio/leftHand.png" alt="Previous" className="w-full h-full object-contain" />
                     </button>
                     <img src={images[imageNumber]} className="w-2/3" alt="Page" />
                     <button 
-                        className={`w-24 h-24 mx-8 animate-bobbing-right ${imageNumber === images.length - 1 ? 'opacity-50 cursor-default' : ''}`} 
+                        className={`w-32 h-32 mx-8 animate-bobbing-right ${imageNumber === images.length - 1 ? 'opacity-50 cursor-default' : ''}`} 
                         onClick={imageNumber < images.length - 1 ? goNextImage : undefined}
                     >
                         <img src="/portfolio/rightHand.png" alt="Next" className="w-full h-full object-contain" />
                     </button>
+                </div>
+                <div className="relative flex flex-col justify-end items-center w-full mb-5 z-10">
+                    <ProjectScroll/>
                 </div>
             </div>
             <Footer />
