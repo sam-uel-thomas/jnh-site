5feecb6c972527a35fdbe0195176b5dba0173551
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-05-23 15:48:39 +0100

    fix hidden gif

--- app/components/landingPage.jsx
+++ app/components/landingPage.jsx
@@ -11,7 +11,7 @@ const LandingPage = () => {
                 <Navbar />
                 <div className="h-full object-cover mt-4 relative">
                     <Image src="/animation/jnhWalking.gif" alt="JNH Rectangle" width={1000} height={1000} className="absolute w-full h-full object-contain object-center -z-2 md:hidden px-2" />
-                    <Image src="/animation/8.GIF" alt="JNH Rectangle" width={2000} height={2000} className="absolute h-full object-contain object-center -z-2 hidden md:block" />
+                    <Image src="/animation/7.GIF" alt="JNH Rectangle" width={2000} height={2000} className="absolute h-full object-contain object-center -z-2 hidden md:block" />
                 </div>
                 <div className="relative flex flex-col justify-end items-center w-full mb-5 z-10">
                     <ProjectScroll/>
