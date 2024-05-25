6fe8cd1b98f1b49178c4c1154a89a07f253bbf61
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-05-25 13:02:39 +0100

    update balenciaga page

--- app/balenciagaMuseumProject/page.jsx
+++ app/balenciagaMuseumProject/page.jsx
@@ -1,30 +1,37 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import ImageCard from '../components/ImageCard'
-import ProjectScroll from '../components/ProjectScroll'
-import balenciagaMuseumProjectData from './balenciagaMuseumProjectData'
-import Link from 'next/link'
+'use client'
+import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import ProjectScroll from '../components/ProjectScroll';
+import ArrowLink from '../components/ArrowLink';
+import Image from 'next/image';
 
-const balenciagaMuseumProject = () => {
+const Page = () => {
+    const images = Array.from({ length: 5 }, (_, i) => `/balenciagaProject/${i + 1}.png`);
+    const [imageNumber, setImageNumber] = useState(0);
+
+    const goPrevImage = () => setImageNumber(Math.max(imageNumber - 1, 0));
+    const goNextImage = () => setImageNumber(Math.min(imageNumber + 1, images.length - 1));
+
     return (
         <div>
-            <div className='bg-white flex flex-col justify-between min-h-[100dvh] overflow-x-hidden'>
+            <div className='bg-white flex flex-col items-center min-h-[100vh] overflow-x-hidden'>
                 <Navbar />
-                <div className="relative flex flex-col justify-end items-center w-full md:w-2/3 mb-5 z-10 pr-8 md:pl-32">
-                    <div className="grid grid-cols-1 md:grid-cols-1 gap-6 md:gap-12 p-6 md:p-16">
-                        {balenciagaMuseumProjectData.length > 0 ? (
-                            balenciagaMuseumProjectData.map((sample, index) => (
-                                <Link href={`/balenciagaMuseumProject/${index}`} key={index}>
-                                    <div className="w-2/3 h-auto">
-                                        <ImageCard src={sample.src} alt={sample.alt} title={index+1} />
-                                    </div>
-                                </Link>
-                            ))
-                        ) : (
-                            <p>No data available</p>
-                        )}
-                    </div>
+                <div className="flex items-center justify-center mx-auto flex-grow">
+                    <ArrowLink href="/portfolio" style={{ top: '100px', left: '20px' }} /> 
+                    <button 
+                        className={`w-32 h-32 mx-8 animate-bobbing-left ${imageNumber === 0 ? 'opacity-0 cursor-default' : ''}`} 
+                        onClick={imageNumber > 0 ? goPrevImage : undefined}
+                    >
+                        <Image src="/portfolio/leftHand.png" alt="Previous" width={50} height={50} className="w-full h-full object-contain" />
+                    </button>
+                    <img src={images[imageNumber]} className="w-1/2" alt="Page" />
+                    <button 
+                        className={`w-32 h-32 mx-8 animate-bobbing-right ${imageNumber === images.length - 1 ? 'opacity-0 cursor-default' : ''}`} 
+                        onClick={imageNumber < images.length - 1 ? goNextImage : undefined}
+                    >
+                        <Image src="/portfolio/rightHand.png" alt="Next" width={50} height={50} className="w-full h-full object-contain" />
+                    </button>
                 </div>
                 <div className="relative flex flex-col justify-end items-center w-full mb-5 z-10">
                     <ProjectScroll/>
@@ -35,4 +42,4 @@ const balenciagaMuseumProject = () => {
     );
 }
 
-export default balenciagaMuseumProject
+export default Page;
