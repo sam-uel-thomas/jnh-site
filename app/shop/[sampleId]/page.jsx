4d4569bb4459b4a131f8d4f22d073f3da1881a08
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-05-28 13:47:29 +0100

    add enquiry note to shop as footer

--- app/shop/[sampleId]/page.jsx
+++ app/shop/[sampleId]/page.jsx
@@ -30,19 +30,21 @@ export default function Page({ params }) {
             <div className='bg-white flex flex-col min-h-screen overflow-x-hidden'>
                 <Navbar />
                 <ArrowLink href="/shop" width={100} height={100} top={20} left={10} />
-                <div className="pl-32 my-28 flex items-start justify-start">
+                <div className="pl-32 mt-28 mb-8 flex items-start justify-start">
                     <div className='relative w-[28rem] h-auto mr-8'>
                         <ImageCard key={photoIndex} src={sample.photos[photoIndex]} alt={sample.alt}/>
                         <button onClick={handlePrevPhoto} className="bg-red-500 rounded-full w-12 h-12 absolute top-1/2 left-0 transform -translate-x-1/2 -translate-y-1/2"></button>
                         <button onClick={handleNextPhoto} className="bg-red-500 rounded-full w-12 h-12 absolute top-1/2 right-0 transform translate-x-1/2 -translate-y-1/2"></button>
                     </div>
-                    <div className='flex flex-col ml-8 w-1/2 mt-12'>
+                    <div className='flex flex-col ml-8 w-1/2 mt-8'>
                         <span className='text-black font-semibold text-4xl mb-8'>{sample.title}</span>
                         {sample.description.map((desc, index) => (
-                            <span key={index} className='text-black font-semibold mb-4 text-2xl'>{desc}</span>
+                            <span key={index} className='text-black font-semibold mb-6 text-2xl'>{desc}</span>
                         ))}
-                        <span className='text-black font-semibold text-2xl mt-4'>Made to order, Enquire through Instagram</span>
                     </div>
+                </div>
+                <div className="self-start mb-12">
+                    <span className='font-semibold text-s mb-4 ml-8 text-left text-black'>MADE TO ORDER + ENQUIRE THROUGH INSTAGRAM</span>
                 </div>
                 <div className='flex flex-row mx-8 mb-16 justify-center'>
                     {shopData.map((item, index) => {
