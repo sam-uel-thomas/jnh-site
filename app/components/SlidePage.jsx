4134eb9a18c1c65f6fdc66891e89d5d7c3a2ae29
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-06-05 10:20:16 +0100

    add fabric samples responsiveness

--- app/components/SlidePage.jsx
+++ app/components/SlidePage.jsx
@@ -1,84 +1,82 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-import ProjectScroll from '../components/ProjectScroll';
 import Link from 'next/link';
 import Image from 'next/image';
-// import LoadingScreen from '../components/LoadingScreen'; // Commented out since it's not used
+import ProjectScroll from '../components/ProjectScroll';
 
 const SlidePage = ({ data, link }) => {
-    const [imageNumber, setImageNumber] = useState(0);
-    const [loading, setLoading] = useState(true);
+  const [imageNumber, setImageNumber] = useState(0);
+  const [loading, setLoading] = useState(true);
 
-    const goPrevImage = () => setImageNumber(Math.max(imageNumber - 1, 0));
-    const goNextImage = () => setImageNumber(Math.min(imageNumber + 1, data.length - 1));
+  const goPrevImage = () => setImageNumber(Math.max(imageNumber - 1, 0));
+  const goNextImage = () => setImageNumber(Math.min(imageNumber + 1, data.length - 1));
 
-    useEffect(() => {
-        const preloadImages = async () => {
-            const imagePromises = data.map((item) => {
-                return new Promise((resolve) => {
-                    const img = new window.Image();
-                    img.src = item.src;
-                    img.onload = resolve;
-                });
-            });
-            await Promise.all(imagePromises);
-            setLoading(false);
-        };
+  useEffect(() => {
+    const preloadImages = async () => {
+      const imagePromises = data.map((item) => {
+        return new Promise((resolve) => {
+          const img = new window.Image();
+          img.src = item.src;
+          img.onload = resolve;
+        });
+      });
+      await Promise.all(imagePromises);
+      setLoading(false);
+    };
 
-        preloadImages();
-    }, [data]);
+    preloadImages();
+  }, [data]);
 
-    // if (loading) {
-    //     return <LoadingScreen />;
-    // }
+  const canGoPrev = imageNumber > 0;
+  const canGoNext = imageNumber < data.length - 1;
 
+  return (
+    <>
+      <div className="flex flex-col px-4 items-center justify-center mx-auto flex-grow md:flex-row md:items-center">
+        <div className="md:hidden flex flex-row justify-center mt-4 order-3">
+          <NavButton onClick={goPrevImage} disabled={!canGoPrev} direction="left">
+            <Image src="/portfolio/leftHand.png" alt="Previous" width={50} height={50} className="w-full h-full object-contain" />
+          </NavButton>
+          <NavButton onClick={goNextImage} disabled={!canGoNext} direction="right">
+            <Image src="/portfolio/rightHand.png" alt="Next" width={50} height={50} className="w-full h-full object-contain" />
+          </NavButton>
+        </div>
+        <div className="order-1 md:order-2">
+          <Link href={`/${link}/${imageNumber}`} className="block">
+            <Image src={data[imageNumber].src} width={700} height={100} alt={data[imageNumber].alt} />
+          </Link>
+        </div>
+        <div className="hidden md:flex md:order-1 md:flex-shrink-0">
+        <NavButton onClick={goPrevImage} disabled={!canGoPrev} direction="left">
+            <Image src="/portfolio/leftHand.png" alt="Previous" width={50} height={50} className="w-full h-full object-contain" />
+        </NavButton>
+        </div>
+        <div className="hidden md:flex md:order-3 md:flex-shrink-0">
+          <NavButton onClick={goNextImage} disabled={!canGoNext} direction="right">
+            <Image src="/portfolio/rightHand.png" alt="Next" width={50} height={50} className="w-full h-full object-contain" />
+          </NavButton>
+        </div>
+      </div>
+      <div className="self-start">
+        <span className='font-semibold text-s mb-4 ml-8 text-left text-orange'>PHOTOGRAPHY : BANDINI3000</span>
+      </div>
+      <div className="relative flex flex-col justify-end items-center w-full mb-5 z-10">
+        <ProjectScroll />
+      </div>
+    </>
+  );
+};
+
+const NavButton = ({ onClick, disabled, children, direction }) => {
     return (
-        <>
-            <div className="flex flex-col px-4 items-center justify-center mx-auto flex-grow md:flex-row md:items-center">
-                <div className="md:hidden flex flex-row justify-center mt-4 order-3">
-                    <button 
-                        className={`w-28 h-28 mx-8 animate-bobbing-left ${imageNumber === 0 ? 'opacity-0 cursor-default' : ''}`} 
-                        onClick={imageNumber > 0 ? goPrevImage : undefined}
-                    >
-                        <Image src="/portfolio/leftHand.png" alt="Previous" width={50} height={50} className="w-full h-full object-contain" />
-                    </button>
-                    <button 
-                        className={`w-28 h-28 mx-8 animate-bobbing-right ${imageNumber === data.length - 1 ? 'opacity-0 cursor-default' : ''}`} 
-                        onClick={imageNumber < data.length - 1 ? goNextImage : undefined}
-                    >
-                        <Image src="/portfolio/rightHand.png" alt="Next" width={50} height={50} className="w-full h-full object-contain" />
-                    </button>
-                </div>
-                <div className="order-1 md:order-2">
-                    <Link href={`/${link}/${imageNumber}`} className="block">
-                        <Image src={data[imageNumber].src} width={700} height={100} alt={data[imageNumber].alt} />
-                    </Link>
-                </div>
-                <div className="hidden md:flex md:order-1 md:flex-shrink-0">
-                    <button 
-                        className={`w-32 h-32 mx-8 animate-bobbing-left ${imageNumber === 0 ? 'opacity-0 cursor-default' : ''}`} 
-                        onClick={imageNumber > 0 ? goPrevImage : undefined}
-                    >
-                        <Image src="/portfolio/leftHand.png" alt="Previous" width={50} height={50} className="w-full h-full object-contain" />
-                    </button>
-                </div>
-                <div className="hidden md:flex md:order-3 md:flex-shrink-0">
-                    <button 
-                        className={`w-32 h-32 mx-8 animate-bobbing-right ${imageNumber === data.length - 1 ? 'opacity-0 cursor-default' : ''}`} 
-                        onClick={imageNumber < data.length - 1 ? goNextImage : undefined}
-                    >
-                        <Image src="/portfolio/rightHand.png" alt="Next" width={50} height={50} className="w-full h-full object-contain" />
-                    </button>
-                </div>
-            </div>
-            <div className="self-start">
-                <span className='font-semibold text-s mb-4 ml-8 text-left text-orange'>PHOTOGRAPHY : BANDINI3000</span>
-            </div>
-            <div className="relative flex flex-col justify-end items-center w-full mb-5 z-10">
-                <ProjectScroll />
-            </div>
-        </>
+      <button 
+        className={`w-32 h-32 mx-8 animate-bobbing-${direction} ${disabled ? 'opacity-0 cursor-default' : ''}`} 
+        onClick={onClick}
+        disabled={disabled}
+      >
+        {children}
+      </button>
     );
-}
+  };
 
 export default SlidePage;
--- app/fabricSamples/page.jsx
+++ app/fabricSamples/page.jsx
@@ -1,99 +1,69 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import ImageCard from '../components/ImageCard'
-import ProjectScroll from '../components/ProjectScroll'
-import Link from 'next/link'
-import fabricSamplesData from '../data/fabricSamplesData'
+'use client';
+import React, { useState, useEffect } from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import ImageCard from '../components/ImageCard';
+import ProjectScroll from '../components/ProjectScroll';
+import Link from 'next/link';
+import fabricSamplesData from '../data/fabricSamplesData';
 
-const fabricSamples = () => {
+const FabricSamples = () => {
+    const [chunkSize, setChunkSize] = useState(3);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 800) {
+                setChunkSize(2); // mobile screens
+            } else {
+                setChunkSize(3); 
+            }
+        };
+
+        handleResize(); // Set initial chunk size
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const chunkArray = (array, size) => {
+        const chunkedArray = [];
+        for (let i = 0; i < array.length; i += size) {
+            chunkedArray.push(array.slice(i, i + size));
+        }
+        return chunkedArray;
+    };
+
+    const rails = chunkArray(fabricSamplesData, chunkSize);
+
     return (
         <div>
             <div className='bg-white flex flex-col min-h-screen overflow-x-hidden'>
                 <Navbar />
-                {/* <div className="relative flex flex-col justify-end items-center w-full md:w-2/3 pt-0 md:pt-0 mb-5 z-10 pr-8 md:pl-32">
-                    <div className="grid grid-cols-1 gap-6 md:gap-12 p-6 md:p-16">
-                        {fabricSamplesData.map((sample, index) => (
-                            <Link href={`/fabricSamples/${index}`} key={index}>
-                                <div className="w-2/3 h-auto">
-                                    <ImageCard src={sample.src} alt={sample.alt} title={index+1} />
-                                </div>
-                            </Link>
-                        ))}
-                    </div>
-                </div> */}
-                <div className="mt-12 relative w-full h-64 mb-64 rotate-2">
-                    <img src="/fabricSample/rail.png" alt="Clothesline" className="absolute w-full" />
-                    <div className="flex justify-between w-full px-8 -mt-4">
-                        <div className="w-1/5">
-                            <Link href="/fabricSamples/0">
-                                <ImageCard src="/fabricSample/1.png" alt="Photo 1"/>
-                            </Link>
+                <div className="mt-12 space-y-32 mb-24">
+                    {rails.map((rail, railIndex) => (
+                        <div key={railIndex} className={`relative w-full mb-64 ${railIndex % 2 === 0 ? 'rotate-2' : '-rotate-2'}`}>
+                            <img src="/fabricSample/rail.png" alt="Clothesline" className="absolute w-full" />
+                            <div className="flex justify-between w-full px-4 md:px-12 lg:px-24 -mt-4 flex-wrap">
+                                {rail.map((sample, sampleIndex) => (
+                                    <div key={sampleIndex} className="w-1/2 md:w-1/3 lg:w-1/4 p-2">
+                                        <Link href={`/fabricSamples/${railIndex * chunkSize + sampleIndex}`}>
+                                            <ImageCard src={sample.src} alt={`Photo ${railIndex * chunkSize + sampleIndex + 1}`} />
+                                        </Link>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
-                        <div className="w-1/5">
-                            <Link href="/fabricSamples/1">
-                                <ImageCard src="/fabricSample/2.png" alt="Photo 2"/>
-                            </Link>
-                        </div>
-                        <div className="w-1/5">
-                            <Link href="/fabricSamples/2">
-                                <ImageCard src="/fabricSample/3.png" alt="Photo 3"/>
-                            </Link>
-                        </div>
-                        <div className="w-1/5">
-                            <Link href="/fabricSamples/3">
-                                <ImageCard src="/fabricSample/4.png" alt="Photo 4"/>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="mt-24 relative w-full h-64 mb-64 -rotate-2">
-                    <img src="/fabricSample/rail.png" alt="Clothesline" className="absolute w-full" />
-                    <div className="flex justify-between w-full px-12 -mt-4">
-                        <div className="w-1/5">
-                            <Link href="/fabricSamples/4">
-                                <ImageCard src="/fabricSample/5.png" alt="Photo 5"/>
-                            </Link>
-                        </div>
-                        <div className="w-1/5">
-                            <Link href="/fabricSamples/5">
-                                <ImageCard src="/fabricSample/6.png" alt="Photo 6"/>
-                            </Link>
-                        </div>
-                        <div className="w-1/5">
-                            <Link href="/fabricSamples/6">
-                                <ImageCard src="/fabricSample/7.png" alt="Photo 7"/>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="mt-24 relative w-full h-64 mb-64 rotate-2">
-                    <img src="/fabricSample/rail.png" alt="Clothesline" className="absolute w-full" />
-                    <div className="flex justify-between w-full px-12 -mt-4">
-                        <div className="w-1/5">
-                            <Link href="/fabricSamples/7">
-                                <ImageCard src="/fabricSample/8.png" alt="Photo 8"/>
-                            </Link>
-                        </div>
-                        <div className="w-1/5">
-                            <Link href="/fabricSamples/8">
-                                <ImageCard src="/fabricSample/9.png" alt="Photo 9"/>
-                            </Link>
-                        </div>
-                        <div className="w-1/5">
-                            <Link href="/fabricSamples/9">
-                                <ImageCard src="/fabricSample/10.png" alt="Photo 10"/>
-                            </Link>
-                        </div>
-                    </div>
+                    ))}
                 </div>
                 <div className="relative flex flex-col justify-end items-center w-full mb-5 z-10 mt-auto">
-                    <ProjectScroll/>
+                    <ProjectScroll />
                 </div>
             </div>
             <Footer />
         </div>
     );
-}
+};
 
-export default fabricSamples
+export default FabricSamples;
