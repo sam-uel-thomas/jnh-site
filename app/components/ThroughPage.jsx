4c0eb9cd8bf123d3f32ab49f3f07f3dced39b4c1
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-06-05 13:32:03 +0100

    update through page

--- app/components/ThroughPage.jsx
+++ app/components/ThroughPage.jsx
@@ -1,7 +1,7 @@
 // components/ProjectContent.jsx
-import React from 'react'
-import ImageCard from './ImageCard'
-import Link from 'next/link'
+import React from 'react';
+import ImageCard from './ImageCard';
+import Link from 'next/link';
 import ArrowLink from './ArrowLink';
 import ProjectScroll from './ProjectScroll';
 
@@ -15,32 +15,32 @@ export default function ProjectContent({ projectData, sampleId, arrowLink }) {
     return (
         <>
             <ArrowLink href={arrowLink} width={100} height={100} top={20} left={10} />
-            <div className="pl-36 mt-12 flex items-start justify-start">
-                <div className='w-[28rem] h-auto mr-8'>
-                    <ImageCard src={sample.src} alt={sample.alt}/>
+            <div className="px-16 md:pl-36 mt-12 flex flex-col md:flex-row items-start justify-start">
+                <div className='w-full md:w-[28rem] h-auto mb-4 md:mb-0 md:mr-8'>
+                    <ImageCard src={sample.src} alt={sample.alt} />
                 </div>
-                <div className='flex flex-col w-1/2'>
+                <div className='flex flex-col w-full md:w-1/2'>
                     <span className='text-black font-semibold text-2xl'>{sample.title}</span>
                     <span className='text-black font-semibold text-xl'>{sample.description}</span>
                 </div>
             </div>
-            <div className='flex flex-row mx-8 mt-12 justify-center'>
+            <div className='grid grid-cols-3 md:grid-cols-none md:flex md:flex-row gap-4 mx-4 md:mx-8 mt-12'>
                 {projectData.map((item, index) => {
                     if (index !== Number(sampleId)) {
                         return (
-                            <div key={index} className='w-[28rem] h-auto mr-8'>
+                            <div key={index} className='w-full h-auto'>
                                 <Link href={`${arrowLink}/${index}`}>
-                                    <ImageCard src={item.src} alt={item.alt}/>
+                                    <ImageCard src={item.src} alt={item.alt} />
                                 </Link>
                             </div>
-                        )
+                        );
                     }
                     return null;
                 })}
             </div>
             <div className="relative flex flex-col justify-end items-center w-full mb-5 z-10 mt-auto">
-                <ProjectScroll/>
+                <ProjectScroll />
             </div>
         </>
-    )
+    );
 }
