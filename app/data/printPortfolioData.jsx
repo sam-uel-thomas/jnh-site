5667da9cd05ddac12a6d8c6784bac090c252ec33
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-06-06 22:08:44 +0100

    convert portfolio to use slide page

---
+++ app/data/printPortfolioData.jsx
@@ -0,0 +1,20 @@
+const printPortfolioData = [
+    { src: "/portfolio/printSamples/1.jpg", alt: "Print Portfolio 1"},
+    { src: "/portfolio/printSamples/2.jpg", alt: "Print Portfolio 2"},
+    { src: "/portfolio/printSamples/3.jpg", alt: "Print Portfolio 3"},
+    { src: "/portfolio/printSamples/4.jpg", alt: "Print Portfolio 4"},
+    { src: "/portfolio/printSamples/5.jpg", alt: "Print Portfolio 5"},
+    { src: "/portfolio/printSamples/6.jpg", alt: "Print Portfolio 6"},
+    { src: "/portfolio/printSamples/7.jpg", alt: "Print Portfolio 7"},
+    { src: "/portfolio/printSamples/8.jpg", alt: "Print Portfolio 8"},
+    { src: "/portfolio/printSamples/9.jpg", alt: "Print Portfolio 9"},
+    { src: "/portfolio/printSamples/10.jpg", alt: "Print Portfolio 10"},
+    { src: "/portfolio/printSamples/11.jpg", alt: "Print Portfolio 11"},
+    { src: "/portfolio/printSamples/12.jpg", alt: "Print Portfolio 12"},
+    { src: "/portfolio/printSamples/13.jpg", alt: "Print Portfolio 13"},
+    { src: "/portfolio/printSamples/14.jpg", alt: "Print Portfolio 14"},
+    { src: "/portfolio/printSamples/15.jpg", alt: "Print Portfolio 15"},
+    { src: "/portfolio/printSamples/16.jpg", alt: "Print Portfolio 16"},
+];
+
+export default printPortfolioData;
--- app/portfolio/portfolioPage/page.jsx
+++ app/portfolio/portfolioPage/page.jsx
@@ -1,47 +1,26 @@
 'use client'
-import React, { useState } from 'react';
-import Navbar from '../../components/Navbar';
-import Footer from '../../components/Footer';
-import ProjectScroll from '../../components/ProjectScroll';
+import React from 'react';
 import { useSearchParams } from 'next/navigation';
-import ArrowLink from '../../components/ArrowLink';
+import printPortfolioData from '../../data/printPortfolioData';
+import mainPortfolioData from '../../data/mainPortfolioData';
+import Navbar from '@/app/components/Navbar';
+import Footer from '@/app/components/Footer';
+import SlidePage from '@/app/components/SlidePage';
 
 const Page = () => {
     const searchParams = useSearchParams();
     const portfolio = searchParams.get('portfolio');
-    const imageCount = portfolio === 'main' ? 28 : 16;
-    const images = Array.from({ length: imageCount }, (_, i) => `/portfolio/${portfolio}/${i + 1}.jpg`);
-    const [imageNumber, setImageNumber] = useState(0);
 
-    const goPrevImage = () => setImageNumber(Math.max(imageNumber - 1, 0));
-    const goNextImage = () => setImageNumber(Math.min(imageNumber + 1, images.length - 1));
+    const data = portfolio === 'main' ? mainPortfolioData : printPortfolioData;
 
     return (
-        <div>
-            <div className='bg-white flex flex-col items-center min-h-[100vh] overflow-x-hidden'>
+        <>
+            <div className='bg-white flex flex-col items-center min-h-[100svh] overflow-x-hidden'>
                 <Navbar />
-                <div className="flex items-center justify-center mx-auto flex-grow">
-                <ArrowLink href="/portfolio" style={{ top: '100px', left: '20px' }} /> 
-                    <button 
-                        className={`w-32 h-32 mx-8 animate-bobbing-left ${imageNumber === 0 ? 'opacity-0 cursor-default' : ''}`} 
-                        onClick={imageNumber > 0 ? goPrevImage : undefined}
-                    >
-                        <img src="/portfolio/leftHand.png" alt="Previous" className="w-full h-full object-contain" />
-                    </button>
-                    <img src={images[imageNumber]} className="w-2/3" alt="Page" />
-                    <button 
-                        className={`w-32 h-32 mx-8 animate-bobbing-right ${imageNumber === images.length - 1 ? 'opacity-0 cursor-default' : ''}`} 
-                        onClick={imageNumber < images.length - 1 ? goNextImage : undefined}
-                    >
-                        <img src="/portfolio/rightHand.png" alt="Next" className="w-full h-full object-contain" />
-                    </button>
-                </div>
-                <div className="relative flex flex-col justify-end items-center w-full mb-5 z-10">
-                    <ProjectScroll/>
-                </div>
+                <SlidePage data={data} />
             </div>
             <Footer />
-        </div>
+        </>
     );
 }
 
