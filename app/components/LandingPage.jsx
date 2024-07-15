11efd6e7c282dbf15268523405d20f9e28e1bdfa
sam thomas <62969375+sam-uel-thomas@users.noreply.github.com>
2024-07-15 21:52:42 +0100

    adjust sizing on landing page

--- app/components/LandingPage.jsx
+++ app/components/LandingPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import ProjectScroll from './ProjectScroll';
 import Navbar from './Navbar';
 import Footer from './Footer';
@@ -7,18 +8,24 @@ import Image from 'next/image';
 const LandingPage = () => {
     return (
         <>
-            <header>
+            <Head>
+                <title>Landing Page</title>
+                <meta name="description" content="Welcome to our landing page. Explore our projects and learn more about us." />
+                <meta property="og:title" content="Landing Page" />
+                <meta property="og:description" content="Welcome to our landing page. Explore our projects and learn more about us." />
+                <meta property="og:type" content="website" />
+                <link rel="canonical" href="https://www.jonahdavies.com/" />
+            </Head>
+            <div className='flex flex-col justify-between h-[100dvh]'>
                 <Navbar />
-            </header>
-            <main className='flex flex-col justify-between h-[100dvh]'>
                 <div className="h-full object-cover mt-4 relative">
-                    <Image src="/logos/logoAnimated.gif" alt="Animated JNH Logo" width={1000} height={1000} className="absolute w-full h-full object-contain object-center -z-2 md:hidden px-2" />
-                    <Image src="/animation/7.GIF" alt="JNH Animation" width={2000} height={2000} className="absolute w-full object-contain object-center -z-2 hidden md:block" />
+                    <Image src="/logos/logoAnimated.gif" alt="Logo Animation" width={1000} height={1000} className="absolute w-full h-full object-contain object-center -z-2 md:hidden px-2" />
+                    <Image src="/animation/7.GIF" alt="Main Animation" width={2000} height={2000} className="absolute w-full object-contain object-center -z-2 hidden md:block" />
                 </div>
                 <div className="relative flex flex-col justify-end items-center w-full mb-5 z-10">
                     <ProjectScroll/>
                 </div>
-            </main>
+            </div>
             <Footer />
         </>
     );
